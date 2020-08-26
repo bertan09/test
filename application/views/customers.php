918148b3761f9f073aa7de9504e2c225606809e0
Bertan <bertantandac@gmail.com>
2020-08-26 17:38:24 +0300

    Müşteriler Düzenlemesi

--- application/views/customers.php
+++ application/views/customers.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="col-sm-10">
                         <div class="form-group">
-                            <label for="customer-name">Müşteri Adı</label>
+                            <label for="customer_name">Müşteri Adı</label>
                             <input type="text" class="form-control" id="customer_name" name="customer_name" placeholder="Müşteri Adı Girin ...">
                         </div>
                     </div>
@@ -35,19 +35,19 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="form-group">
-                            <label for="customer-gsm">Cep Telefonu</label>
+                            <label for="customer_gsm">Cep Telefonu</label>
                             <input type="text" id="customer_gsm" name="customer_gsm" class="form-control" placeholder="Cep Telefonu Girin ...">
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="form-group">
-                            <label for="customer-phone">Sabit Telefon</label>
+                            <label for="customer_phone">Sabit Telefon</label>
                             <input type="text" class="form-control" id="customer_phone" name="customer_phone" placeholder="Sabit Telefon Girin...">
                         </div>
                     </div>
                     <div class="col-sm-4">
                         <div class="form-group">
-                            <label for="customer-fax">Fax Numarası</label>
+                            <label for="customer_fax">Fax Numarası</label>
                             <input type="text" class="form-control" id="customer_fax" name="customer_fax" placeholder="Fax Numarası Girin...">
                         </div>
                     </div>
@@ -55,56 +55,57 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="customer-email">E-Posta</label>
+                            <label for="customer_email">E-Posta</label>
                             <input type="text" class="form-control" id="customer_email" name="customer_email" placeholder="E-Posta Adresi Girin...">
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="customer-identity">TC Kimlik No</label>
+                            <label for="customer_identity">TC Kimlik No</label>
                             <input type="text" class="form-control" id="customer_identity" name="customer_identity" placeholder="T.C. Kimlik No. Girin...">
                         </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-12">
+                    <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="customer-address">Adres</label>
-                            <input type="text" class="form-control" id="customer_address" name="customer_address" placeholder="Adres Girin ...">
+                            <label for="customer_address">Adres</label>
+                            <textarea class="form-control" rows="2" id="customer_address" name="customer_address" placeholder="Adres Girin..."></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="customer_city">İl</label>
+                            <select class="form-control" id="customer_city" name="customer_city">
+                                <option selected>KAHRAMANMARAŞ</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="customer_town">İlçe</label>
+                            <select class="form-control" id="customer_town" name="customer_town">
+                                <option selected>ŞEBİNKARAHİSAR</option>
+                                <option>option 2</option>
+                                <option>option 3</option>
+                                <option>option 4</option>
+                                <option>option 5</option>
+                            </select>
                         </div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="customer_city">İl</label>
-                            <select class="form-control" id="customer_city" name="customer_city">
-                                <option>option 1</option>
-                                <option>option 2</option>
-                                <option>option 3</option>
-                                <option>option 4</option>
-                                <option>option 5</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="customer_town">İlçe</label>
-                            <select class="form-control" id="customer_town" name="customer_town">
-                                <option>option 1</option>
-                                <option>option 2</option>
-                                <option>option 3</option>
-                                <option>option 4</option>
-                                <option>option 5</option>
-                            </select>
-                        </div>
-                    </div>
+
                 </div>
                 <div style="display: none" id="company-detail">
                 <div class="row" >
                     <div class="col-sm-12">
                         <div class="form-group">
-                            <label for="company-name">Firma Adı</label>
+                            <label for="company_name">Firma Adı</label>
                             <input type="text" class="form-control" id="company_name" name="company_name" placeholder="Firma Adı Girin ...">
                         </div>
                     </div>
@@ -112,13 +113,13 @@
                     <div class="row">
                         <div class="col-sm-6">
                             <div class="form-group">
-                                <label for="tax-office">Vergi Dairesi</label>
+                                <label for="tax_office">Vergi Dairesi</label>
                                 <input type="text" class="form-control" id="tax_office" name="tax_office" placeholder="Vergi Dairesi Girin ...">
                             </div>
                         </div>
                         <div class="col-sm-6">
                             <div class="form-group">
-                                <label for="tax-number">Vergi Numarası</label>
+                                <label for="tax_number">Vergi Numarası</label>
                                 <input type="text" class="form-control" id="tax_number" name="tax_number" placeholder="Vergi Numarası ...">
                             </div>
                         </div>
@@ -162,6 +163,7 @@
    $(document).ready(function() {
        $("#addButtton").on("click", function(){
            $('#customerForm').trigger("reset");
+           $("#company-detail").hide();
            toastr.remove();
        });
         var url = '<?= base_url('customers/getAll'); ?>';
@@ -244,9 +246,7 @@
             $("#company-detail").toggle();
         });
 
-        $('#add-customer-modal').on('hidden.bs.modal', function (e) {
-            $("#company-detail").hide();
-        })
+
     } );
     function editCustomer(musteri_id) {
 //        $('.form-control').removeClass('is-invalid').removeClass('is-valid');
